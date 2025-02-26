2aa5aacc887ee37f6d6beb8b7dd2fec11c3d3c13
lamlinhh <lamlinhh@example.com>
2025-02-26 22:03:45 +0700

    day1_footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
         <>
           <Header />
           {children}
+          <Footer/>
         </>
       </body>
     </html>
