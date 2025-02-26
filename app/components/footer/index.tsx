427f98146b231b6cc58496037a170b9f2a381782
NguyenVu <vonguyenvu25032003@gmail.com>
2025-02-26 22:26:02 +0700

    .

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,48 +1,48 @@
-"use client"
+"use client";
+import {
+  FacebookOutlined,
+  TwitterOutlined,
+  YoutubeOutlined,
+} from "@ant-design/icons";
 import { Button, Space } from "antd";
 import styles from "./styles.module.scss";
-import {
-    FacebookOutlined,TwitterOutlined,YoutubeOutlined
-  } from '@ant-design/icons';
-
 
 const Footer = () => {
-    return(
-        <div className={styles.container}>
-            <div className={styles.body}>
-                <div className={styles.content}>
-                    <div className={styles.info}>
-                        
-                        <h2>Travel beyond your imagination, with our Travel Agency!</h2>
-                    </div>
-                    <div className={styles.address}>
-                        <h3>Address</h3>
-                        <p>1080 Brickell Ave</p>
-                        <p>Miami - Florida</p>
-                        <p>U.S. of America</p>
-                        <Space size="middle">
-                            <a href="#" aria-label="Facebook" className={styles.icon}>
-                                <FacebookOutlined />
-                            </a>
-                            <a href="#" aria-label="Twitter" className={styles.icon}>
-                                <TwitterOutlined />
-                            </a>
-                            <a href="#" aria-label="Youtube" className={styles.icon}>
-                                <YoutubeOutlined />
-                            </a>
-                        </Space>
-                    </div>
-                    <div className={styles.contact}>
-                        <h3>Contact</h3>
-                        <Button type="primary" className={styles.emailButton}>
-                            <a href="mailto:info@travel.com">info@travel.com</a>
-                        </Button>
-                        <p>+84 944 373 809</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.body}>
+        <div className={styles.content}>
+          <div className={styles.info}>
+            <h2>Travel beyond your imagination, with our Travel Agency!</h2>
+          </div>
+          <div className={styles.address}>
+            <h3>Address</h3>
+            <p>1080 Brickell Ave</p>
+            <p>Miami - Florida</p>
+            <p>U.S. of America</p>
+            <Space size="middle">
+              <a href="#" aria-label="Facebook" className={styles.icon}>
+                <FacebookOutlined />
+              </a>
+              <a href="#" aria-label="Twitter" className={styles.icon}>
+                <TwitterOutlined />
+              </a>
+              <a href="#" aria-label="Youtube" className={styles.icon}>
+                <YoutubeOutlined />
+              </a>
+            </Space>
+          </div>
+          <div className={styles.contact}>
+            <h3>Contact</h3>
+            <Button type="primary" className={styles.emailButton}>
+              <a href="mailto:info@travel.com">info@travel.com</a>
+            </Button>
+            <p>+84 944 373 809</p>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,33 +1,44 @@
-"use client"
+"use client";
 import { Button } from "antd";
 import styles from "./styles.module.scss";
 import Link from "next/link";
 
-
 const Header = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.body}>
-                <div className={styles.content}>
-                    <div>
-                        <ul className={styles.ul}>
-                            <li><Link href="/">Home</Link></li>
-                            <li><Link href="/">Travel</Link></li>
-                            <li><Link href="/">Pages</Link></li>
-                            <li><Link href="/">Shop</Link></li>
-                            <li><Link href="/">Blog</Link></li>
-                            <li><Link href="/">Contact Us</Link></li>
-                        </ul>
-                    </div>
-                    <div className={styles.button}>
-                        <Button type="primary" size={"middle"}>
-                            Seacrh
-                        </Button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.body}>
+        <div className={styles.content}>
+          <div>
+            <ul className={styles.ul}>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/">Travel</Link>
+              </li>
+              <li>
+                <Link href="/">Pages</Link>
+              </li>
+              <li>
+                <Link href="/">Shop</Link>
+              </li>
+              <li>
+                <Link href="/">Blog</Link>
+              </li>
+              <li>
+                <Link href="/">Contact Us</Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.button}>
+            <Button type="primary" size={"middle"}>
+              Seacrh
+            </Button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
