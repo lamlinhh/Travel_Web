7c6fd81e877779b6208dbbe2b8dcf8192992828c
NguyenVu <vonguyenvu25032003@gmail.com>
2025-02-27 22:07:40 +0700

    Refactor_Search

--- components/CardDetails/index.tsx
+++ components/CardDetails/index.tsx
@@ -1,50 +1,55 @@
-import React from "react";
-import styles from "./styles.module.scss";
-import Image from "next/image";
-import ChiangMai from "@/public/assets/ImageDetail/ChiangMai.svg";
 import {
+  ClockCircleFilled,
   EnvironmentFilled,
   MailFilled,
   MergeFilled,
-  ClockCircleFilled,
-} from "@ant-design/icons";
+} from '@ant-design/icons';
+import Image from 'next/image';
+import styles from './styles.module.scss';
+import CardDetail from '@/type/CardDetail';
 
-const index = () => {
+const index = (props: CardDetail) => {
+  const { name, location, desc, price, img, time, colors } = props;
+
   return (
     <div className={styles.card}>
-      <Image src={ChiangMai} alt="Chiang Mai" className={styles.cardImg} />
+      <Image src={img} alt="Chiang Mai" className={styles.cardImg} />
 
       <div className={styles.cardContent}>
         <div className={styles.infoBar}>
           <span>
-            <ClockCircleFilled style={{ color: "#5d60ee", fontSize: "18px" }} />{" "}
-            1 Week
+            <span style={{ marginRight: '6px' }}>
+              <ClockCircleFilled style={{ color: colors, fontSize: '18px' }} />
+            </span>
+            <span>{time}</span>
           </span>
           <div className={styles.icons}>
-            <span style={{ marginRight: "10px" }}>
-              <MailFilled style={{ color: "#5d60ee", fontSize: "18px" }} />
+            <span style={{ marginRight: '10px' }}>
+              <MailFilled style={{ color: colors, fontSize: '18px' }} />
             </span>
             <span>
-              <MergeFilled style={{ color: "#5d60ee", fontSize: "18px" }} />
+              <MergeFilled style={{ color: colors, fontSize: '18px' }} />
             </span>
           </div>
         </div>
 
-        <h2 className={styles.cardTitle}>Chiang Mai</h2>
+        <h2 className={styles.cardTitle}>{name}</h2>
         <p className={styles.location}>
-          <EnvironmentFilled style={{ color: "#5d60ee", fontSize: "18px" }} />{" "}
-          Thailand
+          <EnvironmentFilled style={{ color: colors, fontSize: '18px' }} />
+          {location}
         </p>
 
-        <p className={styles.description}>
-          Visit the temples and the Chiang Mai Night Bazaar, a huge market
-          located on Chiang Klan Road.
-        </p>
+        <p className={styles.description}>{desc}</p>
 
         <div className={styles.cardFooter}>
-          <button className={styles.detailsBtn}>Details</button>
+          <button
+            className={styles.detailsBtn}
+            style={{ backgroundColor: colors }}
+          >
+            Details
+          </button>
           <p className={styles.price}>
-            <span>$490</span>
+            <span>${price}</span>
           </p>
         </div>
       </div>
