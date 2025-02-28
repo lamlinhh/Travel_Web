7f1b82fb902bb0820d0b45573d7e491af85efc67
NguyenVu <vonguyenvu25032003@gmail.com>
2025-02-28 08:35:46 +0700

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
-import type { NextConfig } from "next";
+/**
+ * @type {import('next').NextConfig}
+ */
+require("dotenv").config();
+const withPugins = require("next-compose-plugins");
+const moment = require("moment-timezone");
+const withImages = require("next-images");
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const modifiedDate = moment()
+  .tz("Asia/Ho_Chi_Minh")
+  .format("DD/MM/YYYY HH:mm:ss");
+
+const nextConfig = {
+  i18n: {
+    locales: ["en", "vi"], // Supported languages
+    defaultLocale: "vi", // Default language
+  },
+  reactStrictMode: false,
+  publicRuntimeConfig: {
+    modifiedDate,
+  },
+  images: {
+    disableStaticImages: true,
+    dangerouslyAllowSVG: true,
+  },
+  output: "standalone",
 };
 
-export default nextConfig;
+const plugins = [withImages];
+module.exports = withPugins(plugins, nextConfig);
