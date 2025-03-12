fcdc89615a13228ebc72c2aaadd2ec9feed37fd4
lamlinhh <lamlinhh@example.com>
2025-03-12 15:24:26 +0700

    .

---
+++ screens/Tour/Tour.tsx
@@ -0,0 +1,12 @@
+import { Yard } from "@/libs";
+import styles from "./styles.module.scss";
+
+const Tour = () => {
+    return(
+        <div className={styles.container}>
+            <Yard></Yard>
+        </div>
+    )
+}
+
+export default Tour;
