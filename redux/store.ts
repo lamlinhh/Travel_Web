7940f97df827688e36c3efc0b74889575de73b9b
lamlinhh <lamlinhh@example.com>
2025-04-12 19:24:24 +0700

    .

--- redux/store.ts
+++ redux/store.ts
@@ -8,7 +8,6 @@ import tourDetailReducer from "./slices/tourDetailSlice";
 import bookTourReducer from "@/redux/slices/bookTourSlice";
 import paymentReducer from "@/redux/slices/paymentSlice";
 import categoriesReducer from "@/redux/slices/categoriesSlice";
-import categoryReducer from "./slices/categorySlice";
 
 export const store = configureStore({
   reducer: {
@@ -21,7 +20,6 @@ export const store = configureStore({
     bookTour: bookTourReducer,
     payment: paymentReducer,
     categorie: categoriesReducer,
-    category: categoryReducer,
   },
 });
 
