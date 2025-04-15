18dbc6075cc56ba7cfb51661dee1a3667962af9f
NguyenVu <vonguyenvu25032003@gmail.com>
2025-04-15 20:09:05 +0700

    .

--- modals/TourModal/index.tsx
+++ modals/TourModal/index.tsx
@@ -37,7 +37,7 @@ const UserModal: React.FC<TourModalProps> = ({ open, onClose, onSuccess }) => {
   const { tourDifficulty } = useSelector(
     (state: RootState) => state.tourDifficulty,
   );
-  console.log("categori", categori);
+
   const handleSubmit = () => {
     form.submit();
   };
